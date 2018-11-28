2943c59268792dc203e880855bb73214f9c8a639
Daniel Spandet Grønbjerg <dsg@commentor.dk>
2018-11-28 14:51:28 +0100

    Add project files.

---
+++ Redis_Example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Redis_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
